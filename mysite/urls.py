fc98534418b40f9001fa4eb5c5e9ffe655d5cb0b
Emiely <alexadner34@outlook.sa>
2022-10-11 19:41:11 +0300

    Changed the url.py file in the Configuration Folder (mysite.settings)

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.contrib          import admin
+from django.urls             import path, include
+
+# For Media (Product Images)
+from django.conf.urls.static import static
+from django.conf             import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('store.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
